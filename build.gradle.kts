353eb50baf55d4cf20b811ef446f73121222670f
churchik <vsevolod106@mail.ru>
2023-03-21 11:20:33 +0300

    1234567

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("args4j:args4j:2.33")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
